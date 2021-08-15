1d0d42a9d8bc50a72ba54ad6addd5f3d108a12b6
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2021-08-15 10:28:57 +0200

    Bump com.stuartsierra/dependency to 1.0.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.stuartsierra/dependency "0.2.0"]]
+                 [com.stuartsierra/dependency "1.0.0"]]
   :profiles {:dev {:plugins [[lein-codox "0.10.3"]]
                    :dependencies [[org.clojure/clojurescript "1.9.908"]]
                    :source-paths ["scripts"]
